e6eb371958cd6a8a47ae7e7f21a8b6e96bb5f586
K. Papadopoulos <kostashope@hotmail.com>
2025-04-25 20:11:15 +0300

    feat: more complex JPQL queries added

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -7,8 +7,10 @@ import gr.aueb.cf.model.TeacherMoreInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class App {
     private final static EntityManagerFactory emf = Persistence.createEntityManagerFactory("school7PU");
@@ -18,22 +20,51 @@ public class App {
 
         em.getTransaction().begin();
 
-        Teacher teacher = Teacher.builder()
-                .firstname("Κώστας")
-                .lastname("Γιαννούτσος")
-                .isActive(true)
-                .build();
+//        Teacher teacher = Teacher.builder()
+//                .firstname("Κώστας")
+//                .lastname("Γιαννούτσος")
+//                .isActive(true)
+//                .build();
+//
+//        TeacherMoreInfo teacherMoreInfo = TeacherMoreInfo.builder()
+//                .dateOfBirth(LocalDateTime.of(2000, 2, 1, 10, 10, 10))
+//                .gender(GenderType.MALE)
+//                .build();
+//
+//        teacher.setTeacherMoreInfo(teacherMoreInfo);
+//        Region region = em.find(Region.class, 1);
+//        teacher.addRegion(region);
+//
+//        em.persist(teacher);
 
-        TeacherMoreInfo teacherMoreInfo = TeacherMoreInfo.builder()
-                .dateOfBirth(LocalDateTime.of(2000, 2, 1, 10, 10, 10))
-                .gender(GenderType.MALE)
-                .build();
+        // merge
+//        String sql = "SELECT t FROM Teacher t WHERE t.lastname = :lastname";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        query.setParameter("lastname", "Ανδρούτσος");
+//        Teacher teacher = query.getSingleResult();
+//        teacher.setIsActive(false);
+//        em.merge(teacher);
 
-        teacher.setTeacherMoreInfo(teacherMoreInfo);
-        Region region = em.find(Region.class, 1);
-        teacher.addRegion(region);
+        // Select
+//        String sql = "SELECT t FROM Teacher t WHERE t.isActive = true AND t.region.title = :regionTitle";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        query.setParameter("regionTitle", "Θεσσαλονίκη");
+//        List<Teacher> teachers = query.getResultList();
+//        teachers.forEach(System.out::println);
 
-        em.persist(teacher);
+
+        // Count των teachers ανά περιοχή
+        String sql = "SELECT r.title, COUNT(t) FROM Region r LEFT JOIN r.teachers t " +
+                "WHERE t.isActive = false OR t.isActive IS NULL GROUP BY r.title";
+
+        TypedQuery<Object[]> query = em.createQuery(sql, Object[].class);
+        List<Object[]> teachersPerRegion = query.getResultList();
+        for (Object[] el : teachersPerRegion) {
+            for (Object item : el) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
         em.getTransaction().commit();
 
 
